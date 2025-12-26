27e5e710df4a34a7be407a861da5afc5128f7f83
bruno santos <bruno4t@hotmail.com>
2025-12-26 11:13:32 -0300

    Initial Commit

---
+++ PostsMedia/PostsMediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PostsMediaApp.swift
+//  PostsMedia
+//
+//  Created by Bruno Carvalho on 26/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PostsMediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
